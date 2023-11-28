66cd97d5ca828a92c6b2a6689203384eadce8c9a
Okafor onyebuchi <onyebu720@gmail.com>
2023-11-28 14:45:21 +0100

    js

---
+++ js/index.js
@@ -0,0 +1,18 @@
+function building(){
+    console.log('hello world');
+}
+
+// var
+// let
+// const
+
+let country='nigeria';
+country='niger'
+console.log('i am from ' + country);
+
+let age= 50;    
+console.log(age);
+
+let name= 'john smith';
+console.log(name);
+
